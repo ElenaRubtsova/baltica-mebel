d8133952195be9eae03545d579f9c3eaa71b67dc
ElenaRubtsova <elena.rub2000@gmail.com>
2023-08-07 15:48:12 +0300

    13160 Новые доработки по разделу материалы

--- fasady-i-materialy/template.php
+++ fasady-i-materialy/template.php
@@ -1,12 +1,29 @@
 <?
 $object = new TrigranHLelementsDisplay([7,8,9,22,24,26]);
+
+/*$result =[];
+$keys = ['key1', 'key2', 'key3']; // Массив с ключами
+$object->processKeys($result, $keys, 'value1');
+$object->processKeys($result, $keys, 'value2');
+
+$keys3 = ['key1', 'key2', 'key4']; // Массив с ключами
+$object->processKeys($result, $keys3, 'value');
+pp($result);*/
 $arResult = $object->MakeArray();
+pp($arResult);
 
 //словарь соответствия названия типа материала его значениям в разных HL блоках (UF_GROUP)
 // pp($object->arMaterialNames);
 
 //массив для вывода
-// pp($arResult);
+ //pp($arResult);
+
+function echoSubGroup() {
+
+}
+function echoGroup() {
+
+}
 ?>
 <div class="row">
 	<div class="col-md-12">
@@ -19,31 +36,71 @@ $arResult = $object->MakeArray();
 							<?=$name?>
 						</span>
 						<span class="element-count muted font_xs rounded3">
-							<?=count($material["ELEMENTS"])?>
+							<?=count($material["ELEMENTS_POD_ZAKAZ"])+count($material["ELEMENTS_V_NALICHII"])?>
 						</span>
 					</div>
 					<div id="mats-accordion<?=$material["GROUP_ID"]?>" class="panel-collapse collapse" style="height: 0px;">
 						<div class="accordion-body">
 							<div class="row">
-								<div class="col-md-12">
-									<div class="row">
-									<?foreach ($material["ELEMENTS"] as $element) {?>
-										<div class="col-xs-6 col-md-2">
-											<div id="mat<?=$element["ID"]?>" class="mats-element bordered box-shadow">
-												<a class="fancy dark_link js-notice-block__title font_sm" data-thumb="<?=$element["PREVIEW_SRC"]?>" data-fancybox ="mats-gall-<?=$material["GROUP_ID"]?>" data-caption="<?=$element["UF_NAME"]?>" title="<?=$element["UF_NAME"]?>" href="<?=$element["IMG_SRC"]?>">
-													<div class="mats-pict">
-														<img class="img-responsive"src="<?=$element["PREVIEW_SRC"]?>">
-													</div>
-													
-													<div class="mats-name">
-														<?=$element["UF_NAME"]?>
-													</div>
-												</a>
-											</div>
-										</div>
-									<?}?>
-									</div>
-								</div>
+                                <div class="col-md-12">
+
+                                <? if (count($material["ELEMENTS_V_NALICHII"]) != 0) { ?>
+                                    <h3>В наличии</h3>
+                                    <div class="row">
+                                        <? foreach ($material["ELEMENTS_V_NALICHII"] as $element) { ?>
+                                            <div class="col-xs-6 col-md-2">
+                                                <div id="mat<?=$element["ID"]?>"
+                                                     class="mats-element bordered box-shadow">
+                                                    <a class="fancy dark_link js-notice-block__title font_sm"
+                                                       data-thumb="<?=$element["PREVIEW_SRC"]?>"
+                                                       data-fancybox="mats-gall-<?=$material["GROUP_ID"]?>"
+                                                       data-caption="<?=$element["UF_NAME"]?>"
+                                                       title="<?=$element["UF_NAME"]?>"
+                                                       href="<?=$element["IMG_SRC"]?>">
+                                                        <div class="mats-pict">
+                                                            <img class="img-responsive"
+                                                                 src="<?=$element["PREVIEW_SRC"]?>">
+                                                        </div>
+
+                                                        <div class="mats-name">
+                                                            <?=$element["UF_NAME"]?>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        <? } ?>
+                                    </div>
+                                <? } ?>
+
+                                <? if (count($material["ELEMENTS_POD_ZAKAZ"]) != 0) { ?>
+                                    <h3>Под заказ</h3>
+                                    <div class="row">
+                                        <? foreach ($material["ELEMENTS_POD_ZAKAZ"] as $element) { ?>
+                                            <div class="col-xs-6 col-md-2">
+                                                <div id="mat<?=$element["ID"]?>"
+                                                     class="mats-element bordered box-shadow">
+                                                    <a class="fancy dark_link js-notice-block__title font_sm"
+                                                       data-thumb="<?=$element["PREVIEW_SRC"]?>"
+                                                       data-fancybox="mats-gall-<?=$material["GROUP_ID"]?>"
+                                                       data-caption="<?=$element["UF_NAME"]?>"
+                                                       title="<?=$element["UF_NAME"]?>"
+                                                       href="<?=$element["IMG_SRC"]?>">
+                                                        <div class="mats-pict">
+                                                            <img class="img-responsive"
+                                                                 src="<?=$element["PREVIEW_SRC"]?>">
+                                                        </div>
+
+                                                        <div class="mats-name">
+                                                            <?=$element["UF_NAME"]?>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        <? } ?>
+                                    </div>
+                                <? } ?>
+
+                                </div>
 							</div>
 						</div><!--accordion-body-->
 					</div><!--panel-collapse-->
--- fasady-i-materialy/tri_class.php
+++ fasady-i-materialy/tri_class.php
@@ -6,18 +6,28 @@
 class TrigranHLelementsDisplay {
 	
 	public $HlIds;
-	public $arMaterialNames;
+    public $ufCodes=[];
+	public $arNameIds=[];
 	public $arResult=[];
 
 	public function __construct($arIds) {
 		$this->HlIds=$arIds;
-		//составляем словарь соответствия названия типа материала его значениям в разных HL блоках (UF_GROUP)
-		$this->GetMaterialsNames('UF_GROUP');
+		//составляем словарь соответствия названия свойства и его значениям в разных HL блоках
+        $props = ['UF_GROUP', 'UF_SUBSECT', 'UF_NALICHIE'];//!to do: можно вынести в параметры компонента (в порядке вложенности разделов)
+        $this->ufCodes = $props;
+        foreach ($props as $prop)
+		    $this->GetPropertyIdsByName($prop);
 	}
-	
-	//составляет словарь
-	public function GetMaterialsNames($ufcode) {
+
+	//составляет массив вида [ "VALUE" => [ "ID" ] ] для свойства типа список
+	public function GetPropertyIdsByName($ufcode) {
 		if($ufcode=='') return false;
+		if($ufcode=='UF_NALICHIE') {
+            $materialEnumIds['В наличии'][]=1;
+            $materialEnumIds['Под заказ'][]=0;
+            $this->arNameIds[$ufcode] = $materialEnumIds;
+        }
+
 		$rsType = CUserFieldEnum::GetList(array(), array(
 			'USER_FIELD_NAME' => $ufcode,
 			));
@@ -25,49 +35,94 @@ class TrigranHLelementsDisplay {
 				$MatName=$arType["VALUE"];
 				$materialEnumIds[$MatName][]=$arType["ID"];
 			}
-		$this->arMaterialNames=$materialEnumIds;
+		$this->arNameIds[$ufcode] = $materialEnumIds;
 	}
-	
+
 	//готовим массив для вывода в шаблоне
 	public function MakeArray() {
 		foreach($this->HlIds as $HlId) {
 			//получаем данные: каждый элемент массива содержит массив с полями HL блока
 			$arData = $this->getDataFromHlblock($HlId, ["UF_MAIN"=>1]);	
-			//группируем по типу материала
-			$this->GroupByType($arData);
+			//группируем по свойствам
+			$this->GroupByProperties($arData);
 			}
-	return $this->arResult;
+	    return $this->arResult;
 	}
 	
-	//Группировка по типу материала (UF_GROUP) и обработка полей 
-	public function GroupByType($data) {
+	//Группировка по свойствам и обработка полей
+	public function GroupByProperties($data) {
 		foreach($data as $element) {
-
-			//получаем название типа материала (UF_GROUP) из словаря по его ID
-			$group_list_item_id=$element["UF_GROUP"];
-			if(!$group_list_item_id) continue;
-			$group_list_item_name=$this->getGroupElementName($group_list_item_id);
+            $group_list_item_names = [];
+            $group_list_item_ids = [];
+            foreach ($this->ufCodes as $ufCode) {
+                /*if($ufcode=='UF_NALICHIE') {
+                    if ($element['UF_NALICHIE'])
+                        $group_list_item_names[] = 'В наличии';
+                    else
+                        $group_list_item_names[] = 'Под заказ';
+                } else {*/
+                    //получаем название свойства из словаря по его ID
+                    $group_list_item_id = $element[$ufCode];
+                    if (!$group_list_item_id) continue;
+                    else $group_list_item_ids[] = $group_list_item_id;
+                    $group_list_item_names[] = $this->getPropertyElementName($ufCode, $group_list_item_id);
+            }
 			
 			//обработка изображения и содание превью 
 			$file = CFile::GetFileArray($element['UF_FILE']);
 			$element["IMG_SRC"]=$file['SRC'];
 			$tmpImg=CFile::ResizeImageGet($element['UF_FILE'], array('width' => 200, 'height' => 200), BX_RESIZE_IMAGE_EXACT, true);
 			$element["PREVIEW_SRC"]=$tmpImg["src"];
-			
-			//сохраняем ID элемента списка для дополнительной маркировки элементов в шаблоне для проверки
-			$this->arResult[$group_list_item_name]["GROUP_ID"]=$group_list_item_id;
-			unset($element["UF_GROUP"]);
-			
-			if($group_list_item_name) $this->arResult[$group_list_item_name]["ELEMENTS"][]=$element;
+
+            /*for ($i = 0; $i < count($group_list_item_names); $i++) {
+                $group_list_item_id = $group_list_item_ids[$i];
+                $group_list_item_name = $group_list_item_names[$i];
+                $ufCode = $this->ufCodes[$i];
+                //сохраняем ID элемента списка для дополнительной маркировки элементов в шаблоне для проверки
+                $this->arResult[$group_list_item_name][$ufCode."_ID"] = $group_list_item_id;
+                unset($element[$ufCode]);
+            }*/
+
+
+            $this->processKeys($this->arResult, $group_list_item_names, $element['UF_NAME']);
+
+            /*foreach ($group_list_item_names as $prop) {
+                if ($lastProp) {
+                    $this->arResult[$lastProp] = [$prop => $element];
+                    $lastProp = $prop;
+                } else {
+                    $this->arResult[$prop] = $element;
+                    $lastProp = $prop;
+                }
+            }
+                //Группировка по наличию/под заказ (UF_NALICHIE)
+                if($group_list_item_name) {
+                    if ($element['UF_NALICHIE'] == true)
+                        $this->arResult[$group_list_item_name]["ELEMENTS_V_NALICHII"][]=$element;
+                    else
+                        $this->arResult[$group_list_item_name]["ELEMENTS_POD_ZAKAZ"][]=$element;
+                }*/
 		}			
 	}
+
+    //добавляет значение в результирующий массив по цепочке переданных ключей
+    function processKeys(&$result, $keys, $value) {
+        $currentArray = &$result;
+        foreach ($keys as $key) {
+            if (!isset($currentArray[$key])) {
+                $currentArray[$key] = [];
+            }
+            $currentArray = &$currentArray[$key];
+        }
+        $currentArray[] = $value;
+    }
 	
-	//возращает название типа материала (UF_GROUP) по ID значения списка из словаря
-	public function getGroupElementName($group_list_item_id) {
-			foreach($this->arMaterialNames as $name=>$arrIds) {
-				if(in_array($group_list_item_id,$arrIds)) return $name;
-			}
-			return false;
+	//возращает название свойства по ID значения списка из словаря
+	public function getPropertyElementName($ufcode, $group_list_item_id) {
+        foreach($this->arNameIds[$ufcode] as $name=>$arrIds) {
+            if(in_array($group_list_item_id,$arrIds)) return $name;
+        }
+        return false;
 	}
 	
 	//Получает все данные из HL блока по его ID и фильтру
